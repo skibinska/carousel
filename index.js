25937c5b54a6ef819b4a806c189b50376d4bd3d8
ewelina <skibinska.ewelina@gmail.com>
2017-04-25 19:49:10 +0100

    Add carousel

---
+++ index.js
@@ -0,0 +1,57 @@
+(function ($) {
+  'use strict';
+
+  $('.carousel').carousel({
+    indicators: true,
+    shift: 100
+  });
+
+  [
+    'lion',
+    'bear',
+    'monkey'
+  ].forEach(function (avatar) {
+    var node = $('.' + avatar);
+    var timeout;
+    if (!node ) return;
+    node.on('tap', function () {
+      animateCheckmark();
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        window.location.href = 'introduction.html';
+      }, 1500);
+      console.log('hello');
+    });
+  });
+
+  [
+    'lion',
+    'bear',
+    'monkey'
+  ].forEach(function (avatar) {
+    var node = document.getElementsByClassName(avatar)[0];
+    var timeout;
+    if (!node ) return;
+    node.addEventListener('click', function () {
+      animateCheckmark();
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        window.location.href = 'introduction.html';
+      }, 1500);
+      console.log('hello');
+    });
+  });
+
+  function animateCheckmark () {
+    anime({
+      targets: '#checkmark path',
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: 'easeInOutSine',
+      opacity: {
+        value: 1,
+        duration: 100
+      }
+    });
+  }
+
+})(jQuery);
