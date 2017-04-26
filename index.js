e0488648ed9114afb82976a745eacbe295b06b46
ewelina <skibinska.ewelina@gmail.com>
2017-04-26 15:44:27 +0100

    Add choose avatar button and logic

--- index.js
+++ index.js
@@ -6,23 +6,23 @@
     shift: 100
   });
 
-  [
-    'lion',
-    'bear',
-    'monkey'
-  ].forEach(function (avatar) {
-    var node = document.getElementsByClassName(avatar)[0];
+  updateAvatar('introduction', 'sleeping-lion');
+
+  $('.choose-avatar').on('click', function () {
     var timeout;
-    if (!node ) return;
-    node.addEventListener('click', function () {
-      animateCheckmark();
-      clearTimeout(timeout);
-      timeout = setTimeout(function () {
-        window.location.href = 'introduction.html';
-      }, 1500);
-      console.log('hello');
-    });
+    var activeAvatar = $('.carousel').find('.carousel-item.active');
+    var avatarClasses = activeAvatar.attr('class');
+    var avatar = avatarClasses.replace(/(bear|monkey|lion)|[^]/g, '$1');
+
+    animateCheckmark();
+    sessionStorage.setItem('avatar', avatar);
+
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      window.location.href = 'introduction.html';
+    }, 1500);
   });
+
 
   function animateCheckmark () {
     anime({
@@ -34,6 +34,14 @@
         duration: 100
       }
     });
+  }
+
+  function updateAvatar (selector, avatarSelector) {
+    var page = document.getElementsByClassName(selector)[0];
+    if (!page) { return; }
+    var avatarImg = page.getElementsByClassName(avatarSelector)[0];
+    var avatar = sessionStorage.getItem('avatar');
+    avatarImg.src = 'assets/' + avatar + '.svg';
   }
 
 })(jQuery);
